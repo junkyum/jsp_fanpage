7d9e5eb6a70856621eaa54ae7cc77b56b9984047
mk <mk@DESKTOP-7N6K8G0>
2016-12-27 22:29:40 +0900

    mk

--- fanpage/src/com/visit/VisitServlet.java
+++ fanpage/src/com/visit/VisitServlet.java
@@ -1,6 +1,7 @@
 package com.visit;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,53 +11,110 @@ import javax.servlet.http.HttpSession;
 
 import com.member.SessionInfo;
 import com.util.MyServlet;
+import com.util.MyUtil;
 
 @WebServlet("/visit/*")
 public class VisitServlet extends MyServlet{
-
 	private static final long serialVersionUID = 1L;
-	
+
+	private SessionInfo info;
+
 	VisitDAO dao=new VisitDAO();
 
+	@Override
 	protected void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("utf-8");
-		
-		HttpSession seccion= req.getSession();
-		SessionInfo info=(SessionInfo)seccion.getAttribute("member");
-		
+
 		String uri=req.getRequestURI();
-		// String cp = req.getContextPath();
-		
-		
+		HttpSession session=req.getSession();
+
+		info = (SessionInfo)session.getAttribute("member");
+
 		if(uri.indexOf("visit.do")!=-1) {
-			forward(req, resp, "/WEB-INF/views/visit/visit.jsp");
+			visit(req,resp);
 		}
 		else if(uri.indexOf("visit_ok.do")!=-1) {
-			
-			VisitDTO dto = new VisitDTO();
-			if(info==null) 
-				dto.setUserId("guest");
-			else
-				dto.setUserId(info.getUserId());
-		
-			dto.setContent(req.getParameter("content"));
-			
-			dao.insertVisit(dto);
-			
-			forward(req, resp, "/WEB-INF/views/visit/visit.jsp");
+			visit_ok(req, resp);
 		}
 		else if(uri.indexOf("delete.do")!=-1) {
-			int num =Integer.parseInt(req.getParameter("num"));
-			String pageNum=req.getParameter("page");
-			
-			dao.deleteVisit(num);
-			
-			forward(req, resp, "/WEB-INF/views/visit/visit.jsp?page="+pageNum);
-			
+			delete(req, resp);
+		}
+		else if(uri.indexOf("update.do")!=-1) {
+			//update(req, resp);
 		}
 	}
+
+	private void visit(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+
+		String cp=req.getContextPath();
+		MyUtil util=new MyUtil();
+
+		String page=req.getParameter("page");
+		int current_page=1;
+		if(page!=null)
+			current_page=Integer.parseInt(page);
+
+		int dataCount;
+		int numPerPage = 6;
+		int total_page;
+
+		dataCount=dao.dataCount();
+
+		total_page = util.pageCount(numPerPage, dataCount);
+		if(current_page>total_page)
+			current_page=total_page;
+
+		int start = (current_page-1)*numPerPage+1;
+		int end=current_page*numPerPage;
+
+		List<VisitDTO> list=dao.listVisit(start, end);
+
+		String listUrl=cp+"/visit/visit.do";
+		String articleUrl=cp+"/visit/visit.do?page="+current_page;
+
+		String paging=util.paging(current_page, total_page, listUrl);
+
+		// visit.jsp 페이지에 넘길 데이터
+		req.setAttribute("member", info);
+		req.setAttribute("list", list);
+		req.setAttribute("dataCount", dataCount);
+		req.setAttribute("page", current_page);
+		req.setAttribute("total_page", total_page);
+		req.setAttribute("articleUrl", articleUrl);
+		req.setAttribute("paging", paging);
+
+		forward(req, resp, "/WEB-INF/views/visit/visit.jsp");
+	}
+	private void visit_ok(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		int num =Integer.parseInt(req.getParameter("num"));
+		String pageNum=req.getParameter("page");
+
+		dao.deleteVisit(num);
+
+		forward(req, resp, "/WEB-INF/views/visit/visit.jsp?page="+pageNum);
+	}
+
+	private void delete(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+
+		int num =Integer.parseInt(req.getParameter("num"));
+		String pageNum=req.getParameter("page");
+
+		dao.deleteVisit(num);
+
+		forward(req, resp, "/WEB-INF/views/visit/visit.jsp?page="+pageNum);
+
+	}
+
 	
-	
-	
+//	private void update(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+//
+//		int num =Integer.parseInt(req.getParameter("num"));
+//		String pageNum=req.getParameter("page");
+//
+//		dao.updateVisit(num);
+//
+//		forward(req, resp, "/WEB-INF/views/visit/visit.jsp?page="+pageNum);
+//
+//	}
 
 }
